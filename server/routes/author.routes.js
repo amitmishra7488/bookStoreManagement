3cbc0924b3b1f472c77fe1a6a155ca76f13d381b
Amit Mishra <amitmishra74880@gmail.com>
2024-02-06 12:56:43 +0530

    Update author.routes.js

--- server/routes/author.routes.js
+++ server/routes/author.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { isAuthor, auth } = require('../middlewares/auth');
-const { launchBook, getBookForAuthor, getAuthorRevenue, bookLaunchNotification, bookUrlGenerator } = require('../controllers/author.controller');
+const { launchBook, getBookForAuthor, getAuthorRevenue, bookUrlGenerator } = require('../controllers/author.controller');
 const router = express.Router();
 
 
@@ -8,7 +8,6 @@ router.post('/add-book', auth, isAuthor, launchBook);
 router.get('/get-books', auth ,isAuthor, getBookForAuthor);
 router.get('/author-revenue', auth, isAuthor, getAuthorRevenue);
 
-router.post('/notify-alluser', bookLaunchNotification);
 
 // book url
 router.get('/:slug',bookUrlGenerator);
