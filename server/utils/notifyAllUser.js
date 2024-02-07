83ead3cb4a78962aecda11cae86b300ec991ccb0
Amit Mishra <amitmishra74880@gmail.com>
2024-02-07 11:46:54 +0530

    Update notifyAllUser.js

--- server/utils/notifyAllUser.js
+++ server/utils/notifyAllUser.js
@@ -1,20 +1,30 @@
 const UserModel = require("../models/user.model");
+const cron = require("node-cron");
 
 const bookLaunchNotification = async (message) => {
   try {
     const allUsers = await UserModel.find();
-
     const batchSize = 2;
     const batches = [];
     for (let i = 0; i < allUsers.length; i += batchSize) {
       batches.push(allUsers.slice(i, i + batchSize));
     }
 
-    // Send emails in batches with a delay of 1 minute
-    for (let batch of batches) {
-      await sendEmailsWithDelay(message, batch);
-      await new Promise((resolve) => setTimeout(resolve, 60000)); // Delay for 25 seconds (25 seconds * 1000 milliseconds)
+    let currentBatchIndex = 0;
+    if (currentBatchIndex === 0) {
+      sendEmailsWithDelay(message, batches[currentBatchIndex]);
+      currentBatchIndex++;
     }
+
+    const cronJob = cron.schedule("* * * * *", () => {
+      const currentBatch = batches[currentBatchIndex];
+      if (currentBatch) {
+        sendEmailsWithDelay(message, currentBatch);
+        currentBatchIndex++;
+      } else {
+        cronJob.stop(); 
+      }
+    });
 
     return { message: "Mail Sent To All Users Successfully" };
   } catch (error) {
@@ -22,19 +32,16 @@ const bookLaunchNotification = async (message) => {
   }
 };
 
-async function sendEmailsWithDelay(message, users) {
+const sendEmailsWithDelay = (message, users) => {
   try {
     console.log(
-      message,
-      "sent to -->",
-      users.map((user) => user.email)
+      message,"sent to -->",users.map((user) => user.email)
     );
   } catch (error) {
     console.error("Error sending emails:", error);
   }
-}
-
+};
 
 module.exports = {
-    bookLaunchNotification
-}
+  bookLaunchNotification,
+};
